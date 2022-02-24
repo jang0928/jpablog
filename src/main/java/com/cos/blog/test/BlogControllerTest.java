7fa2e9d4c955a25bf9532c2183db53c3efa07f79
장문형 <ansgud963@gmail.com>
2022-02-24 15:00:15 +0900

    git Setting

---
+++ src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -0,0 +1,15 @@
+package com.cos.blog.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //특정 어노테이션 붙어있는 클래스 파일을 new 해서 스프링에서 관리
+public class BlogControllerTest {
+
+    @GetMapping("test/hello")
+    public String hello() {
+        return "<h1>Hellog St</h1>";
+    }
+}
